62cb914630b315bbb956c5fbb63551ec28323437
Jacob Gordon <62991719+jpgordon00@users.noreply.github.com>
2021-07-28 03:35:53 -0700

    Update UniqueTypeRegistry.cs

--- UniqueTypeRegistry.cs
+++ UniqueTypeRegistry.cs
@@ -108,7 +108,7 @@ namespace BeastBear {
 			True to match by classname instead of Type
 		*/
 		public virtual bool MatchClassName {
-            get => false;
+            get => true;
         }
 
         // matched by this string or classname
